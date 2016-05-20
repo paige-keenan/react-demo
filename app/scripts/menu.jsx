7971acbfc30e0719ea7e774b4808dca3f16f7826
Paige Keenan <paikeena@usatlrad3007.global.publicisgroupe.net>
2016-05-20 18:42:30 -0400

    init show menu / hide menu state added

--- app/scripts/menu.jsx
+++ app/scripts/menu.jsx
@@ -4,24 +4,59 @@ import ReactDOM from 'react-dom';
 class Menu extends React.Component {
 
   toggle_menu() {
-    open: !this.state.open 
+    open: !this.state.open
+  }
+
+  // Open Menu Function
+  open_menu(event) {
+    event.preventDefault();
+    this.setState({show_menu: true});
+    this.animate_menu();
+  }
+
+  // Close Menu Function
+  close_menu() {
+    this.setState({show_menu: false});
+    this.animate_menu();
+  }  
+
+  // Controls Show/Hide of Menu based on `show_menu` state
+  animate_menu() {
+    
+    this.isAnimating = true;
+
+    if (this.state.show_menu) {
+      console.log('hit');
+      // Open Menu 
+      this.className = "isOpen"
+      this.isAnimating = false; 
+    }
+    // Close Menu
+    else {
+      console.log('hit');
+    }
   }
 
   constructor(props) {
     super(props);
-    this.state = {
-      open: false
-    };
+    this.state = {open: false}
   }
 
+  // Menu Button Renderer
   render() {
-    return <h1>Menu to go here</h1>
+    return (
+      <header>
+        <button      
+          onClick={this.open_menu.bind(this)}>
+          Menu
+        </button>
+      </header> 
+    );
   }
  }
 
+
  export default Menu;
-
-
 
 ReactDOM.render(
   <Menu/>, 
