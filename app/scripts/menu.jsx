5d7915230f3ace03c6184958c81aa19393005282
Paige Keenan <paikeena@usatlrad3007.global.publicisgroupe.net>
2016-05-21 13:49:54 -0400

    toggle show and hide of menu via state

--- app/scripts/menu.jsx
+++ app/scripts/menu.jsx
@@ -3,60 +3,32 @@ import ReactDOM from 'react-dom';
 
 class Menu extends React.Component {
 
-  toggle_menu() {
-    open: !this.state.open
-  }
-
-  // Open Menu Function
-  open_menu(event) {
-    event.preventDefault();
-    this.setState({show_menu: true});
-    this.animate_menu();
-  }
-
-  // Close Menu Function
-  close_menu() {
-    this.setState({show_menu: false});
-    this.animate_menu();
-  }  
-
-  // Controls Show/Hide of Menu based on `show_menu` state
-  animate_menu() {
-    
-    this.isAnimating = true;
-
-    if (this.state.show_menu) {
-      console.log('hit');
-      // Open Menu 
-      this.className = "isOpen"
-      this.isAnimating = false; 
-    }
-    // Close Menu
-    else {
-      console.log('hit');
-    }
-  }
-
+  // Set Inital State and Properties here
   constructor(props) {
     super(props);
-    this.state = {open: false}
+    this.state = {show_menu: false};
+  }  
+
+  // Toggle Menu Function
+  toggle_menu(event) {
+    event.preventDefault();
+    this.setState({show_menu: !this.state.show_menu});
   }
+
 
   // Menu Button Renderer
   render() {
     return (
       <header>
-        <button      
-          onClick={this.open_menu.bind(this)}>
+        <button 
+          className={this.state.show_menu ? 'isOpen' : ''}     
+          onClick={this.toggle_menu.bind(this)}>
           Menu
         </button>
       </header> 
     );
   }
- }
-
-
- export default Menu;
+}
 
 ReactDOM.render(
   <Menu/>, 
