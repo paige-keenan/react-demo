4e18682d0fe0a7f16b5faaf33ec40ed860fc736f
Paige Keenan <paikeena@usatlrad3007.global.publicisgroupe.net>
2016-05-23 16:50:49 -0400

    init setup of looping through sub-nav array

--- app/scripts/nav.jsx
+++ app/scripts/nav.jsx
@@ -7,12 +7,25 @@ export default class Nav extends React.Component {
 
   // Render Links
   render_nav_list_item(links) {
-    return links.map(function(link_item, i){
+    return links.map(function(link_item, i) {
+      // Conditional to check if `hasSubMenu` is TRUE in data    
+      var Sub_Nav = undefined;
+
+      if (link_item.hasSubMenu) {
+        link_item.subNavLinks.map(function(sub_nav_link, i) {
+          Sub_Nav = 
+            <ul key={i}>
+              <li key={i}>{sub_nav_link.subNavTitle}</li>
+            </ul>  
+        });
+      }
+
       return (
         <li key={i}>
           <a href={link_item.url}>
             {link_item.title}
           </a>
+          {Sub_Nav}
         </li> 
       );     
     });
