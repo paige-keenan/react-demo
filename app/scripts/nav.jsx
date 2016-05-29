adf3bdcb03e26b9f3d31fc08e7bd1732d8c2cf0a
Paige Keenan <paikeena@usatlrad3007.global.publicisgroupe.net>
2016-05-29 18:43:38 -0400

    resolve inability to toggle issue on nav by setting state in each component

--- app/scripts/nav.jsx
+++ app/scripts/nav.jsx
@@ -1,57 +1,88 @@
-// Navigation Component
+// Nav Component
 // =============================
 
 import React from 'react';
 
-export default class Nav extends React.Component {
+export default class Link_Item extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      active: false
+    }
+    this.toggle = this.toggle.bind(this);
+  }
 
-  // Render Links
-  render_nav_list_item(links, subMenuClick, subMenuState) {
-    return links.map(function(link_item, i) {
-      var Sub_Nav = undefined;
-      var sub_links = undefined;
-      // Conditional to check if `hasSubMenu` is TRUE in data    
-      if (link_item.hasSubMenu) {
-        sub_links = link_item.subNavLinks.map(function(sub_nav_link, i) {
-          return  <li key={i}>
-                    <a href={sub_nav_link.subNavUrl}>
-                      {sub_nav_link.subNavTitle}
-                    </a>
-                  </li>
-        });
-        // Append the `sub_links` into this `<ul>` and then append to return below
-        Sub_Nav = <ul>{sub_links}</ul>
-      }
-
-      return (
-        <li 
-          key={i}
-          data-has-sub-nav={link_item.hasSubMenu ? true : false}
-          onClick={subMenuClick}>
-          <a 
-            href={link_item.url}>
-            {link_item.title}
-          </a>
-          {Sub_Nav}
-        </li> 
-      );   
-
-          
+  toggle() {
+    this.setState({
+      active: !this.state.active,
+      className: true
     });
-  }  
+  }
 
-       
-
-
-  // Navigation Renderer
+  // Nav List Item Renderer
   render() {
-    return (
-      <nav
-        data-menu-state={this.props.currentState ? 'isOpen' : 'isClosed'}>
-        <ul>
-          {this.render_nav_list_item(this.props.data.links, this.props.clickSubNav, this.props.subMenuState)}
-        </ul>
-      </nav> 
-    );
+    const link = this.props.details;
+    const active_state = this.state.active ? true : false;
+    var sub_links = '';
+    var Sub_Nav = undefined;
+
+    // Conditional to check if `hasSubMenu` is TRUE in data
+    if (link.hasSubMenu) {
+      sub_links = link.subNavLinks.map(function(sub_nav_link, i) {
+        return(
+          <li 
+            key={i}>
+            <a href={sub_nav_link.subNavUrl}>
+              {sub_nav_link.subNavTitle}
+            </a>
+          </li>
+        );  
+      });
+      // Append the `sub_links` into this `<ul>` and then append to return below
+      Sub_Nav = <ul>{sub_links}</ul>
+    }
+
+    return(  
+      <li
+        data-is-active={active_state}>
+        <a
+          onClick={this.toggle} 
+          href={link.url}>
+          {link.title}
+        </a>
+        {Sub_Nav}
+      </li>
+    );  
   }
 }
+
+export default class Nav extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      linkInfo: this.props.data
+    }
+    this.render_link_info = this.render_link_info.bind(this);
+  }
+
+  render_link_info(key) {
+    return( 
+      <Link_Item 
+        key={key} 
+        details={this.state.linkInfo[key]}/>
+    );  
+  }
+
+  // Nav Renderer
+  render() {
+    return(
+      <nav>
+        <ul>
+          // Loop through all data and return link info in this `ul`
+          {Object.keys(this.state.linkInfo).map(this.render_link_info)} 
+        </ul>
+      </nav>
+    );  
+  }
+} 
+
