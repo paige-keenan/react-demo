de11c4792726bb0229ef069a34e19495498e2176
Paige Keenan <paikeena@usatlrad3007.global.publicisgroupe.net>
2016-05-30 17:03:16 -0400

    update headline

--- app/scripts/content.jsx
+++ app/scripts/content.jsx
@@ -10,7 +10,7 @@ export default class Content extends React.Component {
     return (
       <section 
       	className='content'>
-        <h1>This is a responsive React menu</h1>
+        <h1>This is a Responsive React Menu Demo</h1>
         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially un</p>
       </section>  
     );
